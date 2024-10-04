1c098fc779cdff13bbe2771e59d5cdfb2a406c51
pamashar <p.n.masharov@gmail.com>
2024-10-04 21:42:53 +0200

    git remaps added

--- lua/pamashar/settings/remap.lua
+++ lua/pamashar/settings/remap.lua
@@ -5,10 +5,11 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>hs", "<cmd>:set hls!<CR>")
 
 vim.keymap.set("n", "<leader>t", "<cmd>terminal<CR><S-a>")
+vim.keymap.set("n", "<leader>git logline", "<cmd>terminal<CR><S-a>git logline<CR>")
+vim.keymap.set("n", "<leader>git status", "<cmd>terminal<CR><S-a>git status<CR>")
 
 vim.keymap.set("n", "<leader>w", "viw")
 vim.keymap.set("n", "<leader>dw", "viwd")
-vim.keymap.set("n", "<leader>rw", "viwdi")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
