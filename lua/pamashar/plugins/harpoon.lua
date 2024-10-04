8d50c5f2af6f636b25c7aa75d76cbfe1766dfcb8
pamashar <p.n.masharov@gmail.com>
2024-10-04 21:26:26 +0200

    tabline without harpoon added marks fixed

--- lua/pamashar/plugins/harpoon.lua
+++ lua/pamashar/plugins/harpoon.lua
@@ -13,10 +13,13 @@ return {
         local harpoon = require("harpoon")
         harpoon.setup()
 
-        vim.keymap.set("n", "<leader>a", function()
+        vim.keymap.set("n", "<leader>ma", function()
             harpoon:list():add()
             vim.cmd(":do User")
         end)
+        vim.keymap.set("n", "<leader>mn", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<leader>mp", function() harpoon:list():prev() end)
+
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set("n", "<leader>m1", function() harpoon:list():select(1) end)
@@ -29,21 +32,22 @@ return {
         vim.keymap.set("n", "<leader>m8", function() harpoon:list():select(8) end)
         vim.keymap.set("n", "<leader>m9", function() harpoon:list():select(9) end)
 
-        vim.keymap.set("n", "<leader>mn", function() harpoon:list():next() end)
-        vim.keymap.set("n", "<leader>mp", function() harpoon:list():prev() end)
-
         function Harpoon_files()
             local contents = {}
             local marks_length = harpoon:list():length()
-            local current_file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":.")
-            for index = 1, marks_length do
-                local harpoon_file_path = harpoon:list():get(index).value
-                local file_name = harpoon_file_path == "" and "(empty)" or vim.fn.fnamemodify(harpoon_file_path, ':t')
+            if harpoon:list():get(1) == nil then
+                return nil
+            else
+                local current_file_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":.")
+                for index = 1, marks_length do
+                    local harpoon_file_path = harpoon:list():get(index).value
+                    local file_name = harpoon_file_path == "" and "(empty)" or vim.fn.fnamemodify(harpoon_file_path, ':t')
 
-                if current_file_path == harpoon_file_path then
-                    contents[index] = string.format("     %%#HarpoonNumberActive# %s: %%#HarpoonActive#%s", index, file_name)
-                else
-                    contents[index] = string.format("     %%#HarpoonNumberInactive# %s: %%#HarpoonInactive#%s", index, file_name)
+                    if current_file_path == harpoon_file_path then
+                        contents[index] = string.format("     %%#HarpoonNumberActive# %s. %%#HarpoonActive#%s ", index, file_name)
+                    else
+                        contents[index] = string.format("     %%#HarpoonNumberInactive# %s. %%#HarpoonInactive#%s ", index, file_name)
+                    end
                 end
             end
 
@@ -53,7 +57,12 @@ return {
         vim.opt.showtabline = 2
         vim.api.nvim_create_autocmd({ "BufEnter", "BufAdd", "User" }, {
             callback = function()
-                vim.o.tabline = Harpoon_files()
+                local marks = Harpoon_files()
+                if marks ~= nil then
+                    vim.o.tabline = marks
+                else
+                    vim.o.tabline = string.format("%%#HarpoonActive#      %s ",vim.fn.fnamemodify(vim.fn.expand('%'), ':t'))
+                end
                 vim.api.nvim_set_hl(0, "TabLineFill", { fg = "white", bg = bg })
             end
         })
