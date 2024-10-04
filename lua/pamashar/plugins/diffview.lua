d5d3f7a1eaeca33bebdc371bf625621ad8dbc713
pamashar <p.n.masharov@gmail.com>
2024-10-04 21:47:44 +0200

    diff remaps changed

--- lua/pamashar/plugins/diffview.lua
+++ lua/pamashar/plugins/diffview.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>")
-vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>")
+vim.keymap.set("n", "<leader>diff open", "<cmd>DiffviewOpen<CR>")
+vim.keymap.set("n", "<leader>diff close", "<cmd>DiffviewClose<CR>")
 
 return {
     "sindrets/diffview.nvim",
