8c7eaba4771690fcffd506e7d1b9e1b3c6d87741
pamashar <p.n.masharov@gmail.com>
2024-10-05 13:33:33 +0200

    virtual colorcolumn added

--- lua/pamashar/plugins/colorcolumn.lua
+++ lua/pamashar/plugins/colorcolumn.lua
@@ -1,10 +1,10 @@
 return {
     "lukas-reineke/virt-column.nvim",
---    opts = {
---        char = { "|", "." },
---        virtcolumn = "80, 120",
---        highlight = { "WarningMsg", "WarningMsg" }
---    },
+    opts = {
+        char = { "⸽", "⸽" },
+        virtcolumn = "100, 120",
+        highlight = { "WinSeparator", "WinSeparator" }
+    },
 --    config = function()
 --        local column = require("virt-column")
 --        column.setup()
